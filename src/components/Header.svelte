097812f62aa3c1e193733bb08ef264c1f13e6fb2
Yanick Champoux <yanick@babyl.dyndns.org>
2020-07-28 15:49:08 -0400

    tweaks on the style

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,27 @@
 <header>
-  <h1>The Shipyard <span class="subtitle">a <a
-        href="https://shop.groundzerogames.co.uk/rules.html">Full Thrust</a> ship builder</span></h1>
+  <h1>The Shipyard</h1>
+  <h2>a <a
+        href="https://shop.groundzerogames.co.uk/rules.html">Full Thrust</a> ship builder</h2>
   </header>
 
   <style>
 
     header {
+    display: flex;
+    align-items: baseline;
       width: var(--main-width);
       margin-left: auto;
       margin-right: auto;
     }
-  h1 {
+  h1, h2 {
     font-family: Faktos;
     padding: 0px;
     margin: 0px;
   }
 
-  h1 .subtitle {
+  h2 {
+    text-align: right;
+    flex: 1;
     padding-left: 2em;
     font-size: var(--font-scale-12);
   }
